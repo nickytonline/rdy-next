e306397890a226b3f18283257d8c34c19b00392c
Nick Taylor <nick@iamdeveloper.com>
2022-07-29 10:00:12 -0400

    chore: removed Preact for now as the build does not compile with it

--- next.config.js
+++ next.config.js
@@ -75,8 +75,8 @@ module.exports = withBundleAnalyzer({
     if (!dev && !isServer) {
       // Replace React with Preact only in client production build
       Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
+        // 'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        // react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
         'react-dom': 'preact/compat',
       })
